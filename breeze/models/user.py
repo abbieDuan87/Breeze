4d69bd04fd73947f9bd017cf373715c3fcb853a3
minjnlgc <minjing.chen.21@ucl.ac.uk>
2024-11-01 22:22:30 +0000

    implemented main menu, patient main dashboard, patient edit info

--- breeze/models/user.py
+++ breeze/models/user.py
@@ -1,11 +1,32 @@
 class User:
+    """The basic User class
+    """
     def __init__(self, username, password, role):
         self.__username = username
         self.__password = password
         self.__role = role
+        self.__email = None # TODO: do we need email for register?
+        
+    def get_username(self):
+        return self.__username
         
     def get_password(self):
         return self.__password
+    
+    def get_role(self):
+        return self.__role
+    
+    def get_email(self):
+        return self.__email
+
+    def set_username(self, username):
+        self.__username = username
+    
+    def set_password(self, password):
+        self.__password = password
+        
+    def set_email(self, email):
+        self.__email = email
 
     def login(self, input_password):
         return self.get_password() == input_password
