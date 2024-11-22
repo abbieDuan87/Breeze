9ae43b61ab3843c28cb6b43e038c84f198a70aeb
abbieduan <abbiehduan@gmail.com>
2024-11-22 22:05:04 +0000

    chore: fix for patient info edit

--- breeze/utils/cli_utils.py
+++ breeze/utils/cli_utils.py
@@ -1,5 +1,4 @@
 import os
-
 from breeze.utils.calendar_utils import get_colored_status, strip_ansi_codes
 
 
@@ -50,31 +49,6 @@ def show_disabled_account_dashboard_menu(username):
             "Sorry, you can only log out because your account is disabled."
         )
         return False
-
-
-def direct_to_dashboard(message=""):
-    """
-    Helper function to return to the dashboard with a custom message.
-
-    Args:
-        message (str): Custom message to display before the prompt.
-    """
-    if message:
-        print(f"\n{message}")
-    print("Please press B to go back to the dashboard.")
-
-    while True:
-        user_input = input("> ").strip().lower()
-        if user_input == "b":
-            clear_screen()
-            break
-        else:
-            print("Invalid input. Please press B to go back to the dashboard.")
-
-
-def return_to_previous(string, param):
-    if string.strip().lower() == param:
-        return True
 
 
 def print_appointments(appointments=[]):
@@ -128,3 +102,43 @@ def print_appointments(appointments=[]):
 def clear_screen_and_show_banner(banner_str):
     clear_screen()
     print(banner_str)
+    
+def return_to_previous(string, param, message=""):
+    if message:
+        print(f"\n{message}")
+    if string.strip().lower() == param:
+        return True
+
+def direct_to_dashboard(message=""):
+    """
+    Helper function to return to the dashboard with a custom message.
+
+    Args:
+        message (str): Custom message to display before the prompt.
+    """
+    if message:
+        print(f"\n{message}")
+    print("Please press B to go back to the dashboard.")
+
+    while True:
+        user_input = input("> ").strip().lower()
+        if user_input == "b":
+            clear_screen()
+            break
+        else:
+            print("Invalid input. Please press B to go back to the dashboard.")
+
+def check_exit(input_value, exit_message="Exiting without saving changes..."):
+    """
+    Checks if the user input indicates an exit and handles it accordingly.
+
+    Args:
+        input_value (str): The user's input to check.
+        exit_message (str): The message to display when exiting. Defaults to a standard message.
+
+    Returns:
+        bool: True if the user wants to exit, otherwise False.
+    """
+    if input_value.strip().lower() == "x":
+        return True
+    return False
