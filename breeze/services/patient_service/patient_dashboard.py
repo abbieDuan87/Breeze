c2da1481cc75611173caf5b866fe9f04e69fba64
rheahemmadi <rheahemmadi@gmail.com>
2024-11-22 16:54:44 +0000

    changes to patient dashboard

--- breeze/services/patient_service/patient_dashboard.py
+++ breeze/services/patient_service/patient_dashboard.py
@@ -1,28 +1,35 @@
 from breeze.utils.cli_utils import (
     print_system_message,
     show_disabled_account_dashboard_menu,
+    clear_screen
 )
 from breeze.utils.constants import PATIENT_BANNER_STRING
 
 
 def show_patient_dashboard(user, patient_service):
     """Displays the patient dashboard and processes user actions."""
-    print(PATIENT_BANNER_STRING)
 
-    if user.get_is_disabled():
-        return show_disabled_account_dashboard_menu(user.get_username())
+    while True:
+        clear_screen()
+        print(PATIENT_BANNER_STRING)
 
-    print(f"Hi {user.get_username()}!")
-    print("What do you want to do today?")
-    print("[E] Edit my personal information")
-    print("[R] Record my mood for today")
-    print("[J] Journal - enter your journaling text")
-    print("[S] Search for meditation and relaxation exercises")
-    print("[B] Book or cancel an appointment with my MHWP")
-    print("[X] Log out")
+        if user.get_is_disabled():
+            return show_disabled_account_dashboard_menu(user.get_username())
 
-    user_input = input("> ").strip().lower()
-    return handle_user_choice(user_input, patient_service, user)
+        print(f"Hi {user.get_username()}!")
+        print("What do you want to do today?")
+        print("[E] Edit my personal information")
+        print("[R] Record my mood for today")
+        print("[J] Journal - enter your journaling text")
+        print("[S] Search for meditation and relaxation exercises")
+        print("[B] Book or cancel an appointment with my MHWP")
+        print("[X] Log out")
+
+        user_input = input("> ").strip().lower()
+
+        if user_input in ["e", "r", "j", "s", "b", "x"]:
+            if handle_user_choice(user_input, patient_service, user):
+                return True
 
 
 def handle_user_choice(user_input, patient_service, user):
@@ -39,7 +46,4 @@ def handle_user_choice(user_input, patient_service, user):
         case "b":
             patient_service.manage_appointment(user)
         case "x":
-            return True  # Log out
-        case _:
-            print_system_message("Invalid choice. Please try again.")
-    return False
+            return True
