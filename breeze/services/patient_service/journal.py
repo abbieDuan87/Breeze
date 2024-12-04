3dcdf8c3338ee98a2daa72c4ff0aa6a960b027d2
Ben Thompson <ben112003thompson@gmail.com>
2024-12-04 17:54:50 +0000

    Refactoring in login, journal entry, mood entry, and meditation exercises to reduce unnecessary error messages

--- breeze/services/patient_service/journal.py
+++ breeze/services/patient_service/journal.py
@@ -8,34 +8,42 @@ from breeze.utils.cli_utils import (
 from breeze.utils.constants import PATIENT_BANNER_STRING
 from breeze.models.journal_entry import JournalEntry
 
-
-def enter_journal(user, auth_service):
+def print_journal_dashboard(user, title=None, body=None):
     clear_screen()
     print(PATIENT_BANNER_STRING)
     print(f"Hi {user.get_username()} !")
-    print("Write your journal entry below, or enter [X] to exit without saving.")
-    
-    # title of the journal entry
-    print("\nWhat is the title of your entry?")
+    print("Write your journal entry below, or enter [X] to exit without saving.\n")
+    if title: 
+        print(title)
+    if body:
+        print("\n" + body)
+
+def enter_journal(user, auth_service):
+    error = False
     while True:
+        print_journal_dashboard(user)
+        # title of the journal entry
+        print("What is the title of your entry?")
+        if error:
+            print_system_message("Your title is empty! Please try again!")
         journal_title = input("> ").strip()
         if check_exit(journal_title):
             return 
         if not journal_title:
-            print_system_message("Your title is empty! Please try again!")
+            error = True
             continue
         else:
             break
         
     #  body of the journal entry
+    print_journal_dashboard(user, title=journal_title)
     print("\nWrite your journal entry below.")
     journal_body = input("> ").strip()
     if check_exit(journal_body):
         return
-    
-    # Allow the user to add more to their journal entry
-    journal_additions = []
+   
     while True:
+        print_journal_dashboard(user, title=journal_title, body=journal_body)
         print("\nWould you like to add more? Type [S] to save if finished, or continue writing:")
         journal_addition = input("> ").strip()
         if check_exit(journal_addition):
@@ -43,22 +51,16 @@ def enter_journal(user, auth_service):
         if journal_addition.lower() == "s":
             break
         else:
-            journal_additions.append(journal_addition)
-            clear_screen()
-            print(PATIENT_BANNER_STRING)
-            print(journal_title)
-            print(journal_body)
-            print("\n".join(journal_additions))
+            journal_body = journal_body + "\n" + journal_addition
 
     # Combine all parts of the journal entry
-    journal_ent = journal_body + " " + " ".join(journal_additions)
     datetime_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     date_string = datetime.now().strftime("%Y-%m-%d")
     time_string = datetime.now().strftime("%H:%M:%S")
-    new_entry = JournalEntry(journal_title, journal_ent, date_string, time_string)
+    new_entry = JournalEntry(journal_title, journal_body, date_string, time_string)
     # Save the journal entry
     if hasattr(user, "add_journal_entry"):
-        user.add_journal_entry(new_entry.get_id(), journal_title, journal_ent, datetime_str)
+        user.add_journal_entry(new_entry.get_id(), journal_title, journal_body, datetime_str)
         auth_service.save_data_to_file()
         direct_to_dashboard("Journal entry saved!")
     else:
