db5c60046170b898a1f355a8aaa472a42efdba3e
minjnlgc <minjing.chen.21@ucl.ac.uk>
2024-12-05 11:32:07 +0000

    feat: automatically run seeder.py when start the app

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+import subprocess
 from breeze.app import BreezeApp
 
+
+def run_seeder():
+    try:
+        subprocess.run(["python", "data/seeder.py"], check=True)
+        print("Seeder script ran successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error while running seeder.py: {e}")
+
+
 def main():
+    run_seeder()
+
     app = BreezeApp()
     app.run()
+
 
 if __name__ == "__main__":
     main()
